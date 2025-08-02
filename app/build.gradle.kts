a3fa7c2170e4c97ad74776dd83dd63beff2af885
Andrew <58179101+twokoifish@users.noreply.github.com>
2025-08-02 14:46:27 -0400

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,68 +1,87 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "com.scottyab.rootbeer.sample"
+  namespace = "com.scottyab.rootbeer.sample"
+  compileSdk = 35
+  ndkVersion = "27.0.12077973"
 
-    defaultConfig {
-        applicationId = "com.scottyab.rootbeer.sample"
-        versionName = version.toString()
-        versionCode = findProperty("VERSION_CODE").toString().toInt()
-        vectorDrawables.useSupportLibrary = true
+  defaultConfig {
+    applicationId = "com.scottyab.rootbeer.sample"
+    versionName = version.toString()
+    versionCode = findProperty("VERSION_CODE").toString().toInt()
+    vectorDrawables.useSupportLibrary = true
 
-        base.archivesName = "RootBeerSample-$versionName-[$versionCode]"
+    base.archivesName = "RootBeerSample-$versionName-[$versionCode]"
+
+    @Suppress("UnstableApiUsage")
+    externalNativeBuild {
+      cmake {
+        arguments += listOf("-DANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES=ON")
+        // added to improve security of binary #180
+        cFlags("-fPIC")
+        cppFlags("-fPIC")
+      }
+    }
+  }
+
+  buildFeatures {
+    viewBinding = true
+  }
+
+  // check if the keystore details are defined in gradle.properties (this is so the key is not in github)
+  if (findProperty("ROOTBEER_SAMPLE_STORE") != null) {
+    signingConfigs {
+      // from ~/user/.gradle/gradle.properties
+      create("release") {
+        storeFile = file(findProperty("ROOTBEER_SAMPLE_STORE").toString())
+        keyAlias = findProperty("ROOTBEER_SAMPLE_KEY").toString()
+        storePassword = findProperty("ROOTBEER_SAMPLE_STORE_PASS").toString()
+        keyPassword = findProperty("ROOTBEER_SAMPLE_KEY_PASS").toString()
+      }
+    }
+  }
+
+  buildTypes {
+    debug {
+      applicationIdSuffix = ".debug"
+      isMinifyEnabled = false
     }
 
-    buildFeatures {
-        viewBinding = true
+    release {
+      if (rootProject.hasProperty("ROOTBEER_SAMPLE_STORE")) {
+        signingConfig = signingConfigs["release"]
+      }
+
+      isMinifyEnabled = false
     }
+  }
 
-    // check if the keystore details are defined in gradle.properties (this is so the key is not in github)
-    if (findProperty("ROOTBEER_SAMPLE_STORE") != null) {
-        signingConfigs {
-            // from ~/user/.gradle/gradle.properties
-            create("release") {
-                storeFile = file(findProperty("ROOTBEER_SAMPLE_STORE").toString())
-                keyAlias = findProperty("ROOTBEER_SAMPLE_KEY").toString()
-                storePassword = findProperty("ROOTBEER_SAMPLE_STORE_PASS").toString()
-                keyPassword = findProperty("ROOTBEER_SAMPLE_KEY_PASS").toString()
-            }
-        }
-    }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
 
-    buildTypes {
-        debug {
-            applicationIdSuffix = ".debug"
-            isMinifyEnabled = false
-        }
-
-        release {
-            if (rootProject.hasProperty("ROOTBEER_SAMPLE_STORE")) {
-                signingConfig = signingConfigs["release"]
-            }
-
-            isMinifyEnabled = false
-        }
-    }
+  kotlinOptions {
+    jvmTarget = "17"
+  }
 }
 
 dependencies {
-    implementation(project(":rootbeerlib"))
-    // used when testing the snapshot lib
-    // implementation("com.scottyab:rootbeer-lib:0.1.1-SNAPSHOT")
+  implementation(project(":rootbeerlib"))
 
-    implementation(libs.kotlin.coroutines.core)
-    implementation(libs.kotlin.coroutines.android)
+  implementation(libs.kotlin.coroutines.core)
+  implementation(libs.kotlin.coroutines.android)
 
-    implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.core.ktx)
 
-    implementation(libs.androidx.appcompat)
-    implementation(libs.android.google.material)
+  implementation(libs.androidx.appcompat)
+  implementation(libs.android.google.material)
 
-    implementation(libs.nineoldandroids)
-    implementation(libs.beerprogressview)
+  implementation(libs.nineoldandroids)
+  implementation(libs.beerprogressview)
 
-    implementation(libs.timber)
+  implementation(libs.timber)
 }
--- rootbeerlib/build.gradle.kts
+++ rootbeerlib/build.gradle.kts
@@ -1,102 +1,109 @@
 plugins {
-    alias(libs.plugins.android.library)
-    `maven-publish`
-    signing
+  alias(libs.plugins.android.library)
+  `maven-publish`
+  signing
 }
 
 android {
-    namespace = "com.scottyab.rootbeer"
+  namespace = "com.scottyab.rootbeer"
+  compileSdk = 35
+  ndkVersion = "27.0.12077973"
 
-    defaultConfig {
-        ndk {
-            abiFilters.addAll(setOf("x86", "x86_64", "armeabi-v7a", "arm64-v8a"))
-        }
-
-        @Suppress("UnstableApiUsage")
-        externalNativeBuild {
-            cmake {
-                arguments.add("-DANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES=ON")
-                // added to improve security of binary #180
-                cFlags("-fPIC")
-                cppFlags("-fPIC")
-            }
-        }
+  defaultConfig {
+    ndk {
+      abiFilters.addAll(setOf("x86", "x86_64", "armeabi-v7a", "arm64-v8a"))
     }
 
     @Suppress("UnstableApiUsage")
-    testOptions {
-        targetSdk = libs.versions.android.target.sdk.get().toInt()
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-        }
-    }
-
     externalNativeBuild {
-        cmake {
-            path = file("src/main/cpp/CMakeLists.txt")
-        }
+      cmake {
+        arguments += listOf("-DANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES=ON")
+        // added to improve security of binary #180
+        cFlags("-fPIC")
+        cppFlags("-fPIC")
+      }
     }
+  }
 
-    publishing {
-        singleVariant("release") {
-            withSourcesJar()
-            withJavadocJar()
-        }
+  @Suppress("UnstableApiUsage")
+  testOptions {
+    targetSdk = libs.versions.android.target.sdk.get().toInt()
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
     }
+  }
+
+  externalNativeBuild {
+    cmake {
+      path = file("src/main/cpp/CMakeLists.txt")
+    }
+  }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+
+  publishing {
+    singleVariant("release") {
+      withSourcesJar()
+      withJavadocJar()
+    }
+  }
 }
 
 dependencies {
-    testImplementation(libs.junit)
-    testImplementation(libs.mockito)
+  testImplementation(libs.junit)
+  testImplementation(libs.mockito)
 }
 
 publishing {
-    publications {
-        register<MavenPublication>("release") {
-            artifactId = findStringPropertyOrDefault("POM_ARTIFACT_ID")
+  publications {
+    register<MavenPublication>("release") {
+      artifactId = findStringPropertyOrDefault("POM_ARTIFACT_ID")
 
-            afterEvaluate {
-                from(components["release"])
-            }
+      afterEvaluate {
+        from(components["release"])
+      }
 
-            pom {
-                name = findStringPropertyOrDefault("POM_NAME")
-                packaging = findStringPropertyOrDefault("POM_PACKAGING")
-                description = findStringPropertyOrDefault("POM_DESCRIPTION")
-                url = findStringPropertyOrDefault("POM_URL")
+      pom {
+        name = findStringPropertyOrDefault("POM_NAME")
+        packaging = findStringPropertyOrDefault("POM_PACKAGING")
+        description = findStringPropertyOrDefault("POM_DESCRIPTION")
+        url = findStringPropertyOrDefault("POM_URL")
 
-                scm {
-                    url = findStringPropertyOrDefault("POM_SCM_URL")
-                    connection = findStringPropertyOrDefault("POM_SCM_CONNECTION")
-                    developerConnection = findStringPropertyOrDefault("POM_SCM_DEV_CONNECTION")
-                }
-
-                licenses {
-                    license {
-                        name = findStringPropertyOrDefault("POM_LICENCE_NAME")
-                        url = findStringPropertyOrDefault("POM_LICENCE_URL")
-                        distribution = findStringPropertyOrDefault("POM_LICENCE_DIST")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id = findStringPropertyOrDefault("POM_DEVELOPER_ID")
-                        name = findStringPropertyOrDefault("POM_DEVELOPER_NAME")
-                        organizationUrl = findStringPropertyOrDefault("POM_URL")
-                    }
-                }
-            }
+        scm {
+          url = findStringPropertyOrDefault("POM_SCM_URL")
+          connection = findStringPropertyOrDefault("POM_SCM_CONNECTION")
+          developerConnection = findStringPropertyOrDefault("POM_SCM_DEV_CONNECTION")
         }
+
+        licenses {
+          license {
+            name = findStringPropertyOrDefault("POM_LICENCE_NAME")
+            url = findStringPropertyOrDefault("POM_LICENCE_URL")
+            distribution = findStringPropertyOrDefault("POM_LICENCE_DIST")
+          }
+        }
+
+        developers {
+          developer {
+            id = findStringPropertyOrDefault("POM_DEVELOPER_ID")
+            name = findStringPropertyOrDefault("POM_DEVELOPER_NAME")
+            organizationUrl = findStringPropertyOrDefault("POM_URL")
+          }
+        }
+      }
     }
+  }
 }
 
 signing {
-    sign(publishing.publications["release"])
+  sign(publishing.publications["release"])
 }
 
 private fun Project.findStringPropertyOrDefault(propertyName: String, default: String? = "") =
-    findProperty(propertyName)?.toString() ?: default
+  findProperty(propertyName)?.toString() ?: default
